1d365e119b5eb308243be1fa5d10ba5c8bb5cad9
phamngocminhquan <quan107652@donga.edu.vn>
2024-10-29 22:21:27 +0700

    Add files via upload

---
+++ baitap41.java
@@ -0,0 +1,13 @@
+package phamngocminhquan;
+
+public class baitap41 {
+    
+        public static void main(String[] args) {
+            int arr[] = {3, 5, 8, 10, 14, 25};
+    
+            System.out.println("Các phan tu cua mang arr là:");
+            for (int i = 0; i < arr.length; i++) {
+                System.out.print(arr[i] + " ");
+            }
+    }
+}
