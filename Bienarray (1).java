65d84a04975900969e0b0fa692e7b0644031dce9
phamngocminhquan <quan107652@donga.edu.vn>
2024-10-29 19:32:40 +0700

    Add files via upload

---
+++ Bienarray (1).java
@@ -0,0 +1,20 @@
+package phamngocminhquan;
+
+/**
+ * Bienarray
+ */
+class Bienarray {
+
+    public static void main(String[] args) {
+        int arr2[][] = {{1,2,3},{4,5,6},{7}};
+
+        System.out.println(arr2[0][0]);
+        System.out.println(arr2[0][1]);
+        System.out.println(arr2[0][2]);
+        System.out.println(arr2[1][0]);
+        System.out.println(arr2[1][1]);
+        System.out.println(arr2[1][2]);
+        System.out.println(arr2[2][0]);
+
+    }
+}
