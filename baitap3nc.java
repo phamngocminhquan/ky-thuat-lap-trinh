fa74f996dab77e92c4ddd850b17a198cbc7af3ef
phamngocminhquan <quan107652@donga.edu.vn>
2024-10-30 10:24:12 +0700

    Add files via upload

---
+++ baitap3nc.java
@@ -0,0 +1,23 @@
+package phamngocminhquan;
+
+import java.util.Scanner;
+
+public class baitap3nc {
+    
+    public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+       
+       System.out.println("Nhap mot so nguyen: ");
+       int n = scanner.nextInt();
+
+       for (int i = 1; i <= n; i ++){
+        if (i % 2 !=0) {
+
+            System.out.println(i + " ");
+            
+        }
+       }
+       scanner.close();
+    }
+    
+}
