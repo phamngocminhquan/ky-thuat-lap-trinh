53c9875fae5885676216606d89232284d7a5933a
phamngocminhquan <quan107652@donga.edu.vn>
2024-10-30 10:28:16 +0700

    Add files via upload

---
+++ baitap6nc.java
@@ -0,0 +1,22 @@
+package phamngocminhquan;
+
+import java.util.Scanner;
+
+public class baitap6nc {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Nhap mot so nguyen: ");
+        int n = scanner.nextInt();
+
+        for (int i = 2; i < n; i +=2)
+            if ( i % 2 == 0)
+            {
+                System.out.println(i + " ");
+            }
+        scanner.close();
+    }
+
+    
+}
